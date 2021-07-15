b25e851aaec7f7813dbde51a934a7cca1954b6bb
krishpranav <krisna.pranav@gmail.com>
2021-07-15 15:05:34 +0530

    lib: redditosint comment functions

--- lib/redditosint/comment.rb
+++ lib/redditosint/comment.rb
@@ -1,13 +1,36 @@
+#!/usr/bin/env ruby
+# align comments in the target reddit post
+
+# requires
 require 'time'
 
-class Redditosint::Comment
-    attr_reader :author, :subreddit, :permalink, :timestamp, :text
+class Strigil::Comment
+  attr_reader :author, :subreddit, :permalink, :timestamp, :text
 
-    def initialize(params={})
-        @author = params[:author]
-        @subreddit = params[:subreddit]
-        @permalink = params[:permalink]
-        @timestamp = Time.parse(params[:timestamp])
-        @text = params[:text]
-    end
-    
+  def initialize(params={})
+    @author = params[:author]
+    @subreddit = params[:subreddit]
+    @permalink = params[:permalink]
+    @timestamp = Time.parse(params[:timestamp])
+    @text = params[:text]
+  end
+
+  def to_json
+    {
+      author: author,
+      subreddit: subreddit,
+      permalink: permalink,
+      timestamp: timestamp,
+      text: text
+    }.to_json
+  end
+
+  def display
+    %{
+      posted by #{author} on /r/#{subreddit}
+      #{timestamp} | #{permalink}
+      -------------------------------------
+      #{text}
+    }
+  end
+end

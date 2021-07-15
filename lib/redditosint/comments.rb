8694a845d5e057f9b52ab1fac3827953e6513ced
krishpranav <krisna.pranav@gmail.com>
2021-07-15 15:09:36 +0530

    save logs

--- lib/redditosint/comments.rb
+++ lib/redditosint/comments.rb
@@ -16,4 +16,13 @@ class Rubyosint::Comments
     def add_comment(comment)
         @log.push(comment)
     end
+
+    def save
+        unless File.exists?('./data/')
+            FileUtils.mkdir('./data/')
+    end
+    File.open("./datat/comments.json", 'w') do |f|
+        f.write(log_to_json)
+    end
+end
     

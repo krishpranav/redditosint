0bbc6c54551e0867434be038c69d19ac4f3b0fd0
krishpranav <krisna.pranav@gmail.com>
2021-07-15 15:29:59 +0530

    lib: redditosint login to reddit

--- lib/redditosint/redditosint_client.rb
+++ lib/redditosint/redditosint_client.rb
@@ -19,4 +19,44 @@ class Redditosint::RedditClient
 
         client.navigate.to "https://reddit.com/u/#{target}/comments"
     end
+
+    def pull_comments
+        Redditosint::CommentsParser.parse(client)
+    end
+
+    def close
+        client.close
+    end
+
+    def iterate
+        begin
+            client.find_element(link_text: "next >").click
+        rescue
+            raise EndOfQueueError
+        end
+    end
+
+    def login_and_make_legacy(username, password)
+        login_to_reddit(username, password)
+        set_legacy_preferance
+    end
     
+    def login_to_reddit(username, password)
+        client.navigate.to 'https://reddit.com/login'
+    
+        login_panel = client.find_element(id: "login-form")
+    
+        username_field = login_panel.find_element(id: "user_login")
+        password_field = login_panel.find_element(id: "passwd_login")
+        login_button = login_panel.find_element(tag_name: "button", class: "c-btn")
+    
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+    
+        login_button.click
+    
+        wait = Selenium::WebDriver::Wait.new(timeout: 5)
+        wait.until do
+          client.find_element(id: "header-bottom-right").find_element(link_text: username)
+        end
+      end

c620ace71f22e7ccdf7825f2a78426c28a9631ab
krishpranav <krisna.pranav@gmail.com>
2021-07-15 15:11:03 +0530

    lib: redditosint comments

--- lib/redditosint/comments.rb
+++ lib/redditosint/comments.rb
@@ -1,28 +1,45 @@
 #!/usr/bin/env ruby
 
+# requires
 require 'fileutils'
 
-class Rubyosint::Comments
-    attr_accessor :log
+class Redditosint::Comments
+  attr_accessor :log
 
-    def initialize
-        @log = []
-    end
+  def initialize
+    @log = []
+  end
 
-    def add_comments(comments)
-        @log += comments
-    end
+  def add_comments(comments)
+    @log += comments
+  end
 
-    def add_comment(comment)
-        @log.push(comment)
-    end
+  def add_comment(comment)
+    @log.push(comment)
+  end
 
-    def save
-        unless File.exists?('./data/')
-            FileUtils.mkdir('./data/')
+  def save
+    unless File.exist?('./data/')
+      FileUtils.mkdir('./data/')
     end
-    File.open("./datat/comments.json", 'w') do |f|
-        f.write(log_to_json)
+    File.open("./data/comments.json", 'w') do |f|
+      f.write(log_to_json)
     end
+  end
+
+  def clear
+    @log = []
+  end
+
+  private
+
+  def log_to_json
+    result = '['
+    log.each do |comment|
+      result << comment.to_json
+      result << ',' unless comment == log.last
+    end
+    result << ']'
+    result 
+  end
 end
-    

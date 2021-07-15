6988d0ddf057cade7b94854b75d6ede79ccf4290
krishpranav <krisna.pranav@gmail.com>
2021-07-15 15:41:28 +0530

    lib: redditosint

--- lib/redditosint.rb
+++ lib/redditosint.rb
@@ -1,46 +1,62 @@
-#!/usr/bin/evn ruby
-
-class Rubyosint
+class Redditosint
     def self.engage(user)
-        client_config
-
-        puts "Initializing client..."
-        client = Rubyosint::RubyosintClient.new(
-            username: @username,
-            password: @password,
-            browser: @browser,
-            target: user
-        )
-
-        puts "Initialize comments store..."
-        comments = Rubyosint::Comments.new
-
-        puts "Beginning scrape. This may take some time...."
-        processing = true
-        while processing == true
-            comments.add_comments(client.pull_comments)
-
-            begin
-                client.iterate
-            rescue EndOfQueueError
-                processing = false
-
-
-                client.close
-                commments.save
-                
-                puts "Pulled #{comments.log.size} comments.."
-                puts "JSON data stored in ./data/comments.json"
-
-                comments.clear
-            end
+      client_config
+  
+      puts "Initializing client..."
+      client = Redditosint::RedditosintClient.new(
+        username: @username,
+        password: @password,
+        browser: @browser,
+        target: user
+      )
+  
+      puts "Initializing comments store..."
+      comments = Redditosint::Comments.new
+  
+      puts "Beginning scrape. This may take a while."
+      processing = true
+      while processing == true
+        comments.add_comments(client.pull_comments)
+  
+        begin
+          client.iterate
+        rescue EndOfQueueError
+          processing = false
+  
+          client.close
+          comments.save
+  
+          puts "Pulled #{comments.log.size} comments."
+          puts "JSON data stored in ./data/comments.json"
+  
+          comments.clear
         end
+      end
     end
-    
-
-
-# requires
+  
+    private
+  
+    def self.client_config
+      puts "Valid Reddit account details are neccessary to scrape correctly."
+      puts "This information is _not_ sent to any third party - it is simply"
+      puts "used to properly configure how user comments are displayed in"
+      puts "order to scrape them correctly. Feel free to use a throwaway."
+      puts "Username:"
+      print "> "
+      @username = gets.chomp
+      puts "Password:"
+      print "> "
+      @password = gets.chomp
+      puts "Do you have Chrome or Firefox installed?"
+      puts "Type either 'chrome' or 'firefox' without quotes."
+      @browser = gets.chomp
+    end
+  end
+  
+  class ConfigurationError < StandardError
+  end
+  
 require 'redditosint/comment'
 require 'redditosint/comments'
 require 'redditosint/comments_parser'
-require 'redditosint/strigil_client'
+require 'redditosint/redditosint_client'
